254b6e833159ccd92ee61d80c8efa01c3241189b
Erick <barbosaerick88@gmail.com>
2021-11-24 15:48:04 -0300

    alert_functon_array

---
+++ js/aula7/script.js
@@ -0,0 +1,21 @@
+//alert('teste')
+var nome = 'erick'
+var idade = 29;
+var idade2 = 10;
+var frase = "Japao é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos")
+//alert(idade+idade2)
+console.log(nome)
+console.log(idade + idade2)
+
+//troca japao por brasil
+//console.log(frase.replace("Japao", "Brasil"))
+
+// tudo maiusculo
+console.log(frase.toUpperCase())
+
+//tudo minusculo
+//console.log(frase.toLowerCase())
+
+
+//alert(frase.replace("Japao", "Brasil"))
---
+++ js/aula8/script.js
@@ -0,0 +1,51 @@
+/*var lista = ['Maçã', 'Banana', 'pera'];
+//adciona
+//lista.push("uva")
+
+//tira o ultimo
+//lista.pop();
+
+//reverse lista
+console.log(lista.reverse())
+
+console.log(lista) // 012
+// primeiro '' element da string 
+console.log(lista.toString()[0]) // 012
+
+// troca o , da sting por qualquer parametro
+console.log(lista.join('-'))*/
+
+/*var frutas = [{ nome: 'maça', cor: "vermelha" }, { nome: 'uva', cor: "verde" }]
+console.log(frutas)
+alert(frutas[1].cor)
+*/
+
+/*
+var idade = prompt("qual sua idade")
+if (idade >= 18) {
+  alert("Maior de idade")
+} else {
+  alert("Menor de idade")
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5) {
+  console.log(count)
+  alert(count)
+  count++
+}*/
+
+/*var count;
+for (count = 1; count <= 5; count++)
+  alert(count)
+console.log(count)
+*/
+
+var d = new Date();
+console.log(d.getMonth() + 1)
+console.log(d.getDay())
+console.log(d.getUTCDay())
+console.log(d.getHours())
+//alert(d.getDay())
